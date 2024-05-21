f3febdcd3e3f7a931b94640ebb95ed5245766a6d
Ivanvstark <126107159+IvanVStark@users.noreply.github.com>
2024-05-21 15:59:14 -0500

    Adding some numbers 1
    Adding some numbers 1 complete

---
+++ python_beginning/1_adding_some_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+num = 100
+print(f"Learning Python is fun\"\' - {num} %")
+print("Learning Python is fun\"\' - {} %".format(num))
+print("Learning Python is fun\"\' - " + "%d %%" % num)
