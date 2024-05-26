5a93e87b8b2863f5cdaafd13422ff1f87521de32
Ivanvstark <126107159+IvanVStark@users.noreply.github.com>
2024-05-25 19:59:27 -0500

    1.4 Done
    Finishing the 1.4 Python assignment

---
+++ python_beginning/5_slice_that.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+a = "Webdriver Awesome Camp"
+print(*a.split(),  sep='\n')
+
+
---
+++ python_beginning/6_what_needed.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+a = "There should be one--and preferably only one--obvious way to do it. Ithough that way may not be obvious at first unless you're Dutch."
+print(a[25:44]  + a[53:58] + a[-20:-14])
